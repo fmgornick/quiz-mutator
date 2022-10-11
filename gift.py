b2a28e8c6f8ce7723640f26e412174c2b29f8187
Fletcher Gornick <fletchermgornick@gmail.com>
2022-10-11 08:57:25 -0500

    changed markdown to text file

--- gift.py
+++ gift.py
@@ -1,6 +1,5 @@
 import re
-import shutil
-from pathlib import Path
+from typing import TextIO
 
 import problem_generator as pg
 
@@ -24,31 +23,22 @@ class GIFT:
 
             case "mutation":
                 for set in self.quiz.sets:
-                    new_section(set)
-                shutil.make_archive(self.output, "zip", "package")
-                shutil.rmtree("package")
+                    order(set, f)
+                    multiple_choice(set, f, "findMutation")
+                    multiple_choice(set, f, "classifyMutation")
+                    multiple_choice(set, f, "fixMutation")
+                    f.write("\n\n")
+        f.close()
 
-
-def new_section(set: pg.ProblemSet):
-    filename = "package/" + set.id + ".md"
-    Path(filename).touch()
-    order(set, filename)
-    multiple_choice(set, filename, "findMutation")
-    multiple_choice(set, filename, "classifyMutation")
-    multiple_choice(set, filename, "fixMutation")
-
-
-def order(set: pg.ProblemSet, filename: str):
-    f = open(filename, "a")
+def order(set: pg.ProblemSet, f: TextIO):
     f.write("::" + escape(set.id) + " Ordering Problem\n")
     f.write("::" + escape(set.order.prompt) + " {\n")
 
     for i, line in enumerate(set.order.answer):
         f.write("\t={num} -> {line}\n".format(num=i, line=escape(line)))
     f.write("}\n\n")
-    f.close()
 
-def multiple_choice(set: pg.ProblemSet, filename: str, mc_field: str):
+def multiple_choice(set: pg.ProblemSet, f: TextIO, mc_field: str):
     match mc_field:
         case "findMutation":
             field: pg.Question = set.findMutation
@@ -63,14 +53,12 @@ def multiple_choice(set: pg.ProblemSet, filename: str, mc_field: str):
             print("unreachable")
             return
 
-    f = open(filename, "a")
     f.write("::" + escape(set.id) + escape(title) + "\n")
     f.write("::" + escape(field.prompt) + " {\n")
     f.write("\t=" + escape(field.answer) + "\n")
     for distractor in field.distractors:
         f.write("\t~" + escape(distractor) + "\n")
     f.write("}\n\n")
-    f.close()
 
 def escape(line: str) -> str:
     return re.sub(
