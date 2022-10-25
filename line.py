72b60080e9b866cf30b72ae6562d3ea5d50db1dc
Fletcher Gornick <fletchermgornick@gmail.com>
2022-10-24 20:21:27 -0500

    added representation of a line

--- line.py
+++ line.py
@@ -5,3 +5,10 @@ class Line:
     def __init__(self, code: str, comment: List[str]):
         self.code = code
         self.comment = comment
+
+    def __repr__(self) -> str:
+        line = ""
+        for c in self.comment:
+            line += c
+        line += self.code
+        return line
